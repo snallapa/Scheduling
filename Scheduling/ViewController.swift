57642b420e22086c25984b13189bb318a05d0cbc
Sahith Nallapareddy <sahith@Sahiths-MBP.home>
2015-04-22 15:23:43 -0400

    parse added

--- Scheduling/ViewController.swift
+++ Scheduling/ViewController.swift
@@ -7,11 +7,17 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let testObject = PFObject(className: "TestObject")
+        testObject["foo"] = "bar"
+        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+            println("Object has been saved.")
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
