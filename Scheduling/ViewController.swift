2b197d28e5981a3eb8dec63720be6fab5408b256
Sahith Nallapareddy <sahith@Sahiths-MBP.home>
2015-04-22 13:50:15 -0400

    first commit

---
+++ Scheduling/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scheduling
+//
+//  Created by Sahith Nallapareddy on 4/22/15.
+//  Copyright (c) 2015 Sahith Nallapareddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
