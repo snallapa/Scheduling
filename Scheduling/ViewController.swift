dc4b38571f94020c3d5af9f3960558265c22673d
Sahith Nallapareddy <sahith@AUTOREG-676805.WIFI.WPI.EDU>
2015-04-28 10:06:48 -0400

    fixed users

--- Scheduling/ViewController.swift
+++ Scheduling/ViewController.swift
@@ -10,10 +10,37 @@ import UIKit
 import Parse
 
 class ViewController: UIViewController {
+    
+    var residentsFiltered = [AnyObject]?()
 
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func groupChosen(sender: UIButton) {
+        let color = sender.currentTitle!.lowercaseString
+        var query = PFQuery(className:"Residents")
+        query.whereKey("color", equalTo:color)
+        query.findObjectsInBackgroundWithBlock {
+            (residentsFiltered, error: NSError?) -> Void in
+            if error == nil {
+                // The find succeeded.
+                println("Successfully retrieved \(residentsFiltered!.count) scores.")
+                // Do something with the found objects
+                if let objects = residentsFiltered as? [PFObject] {
+                    for object in objects {
+                        println(object["name"]!)
+                    }
+                }
+            } else {
+                // Log details of the failure
+                println("Error: \(error!) \(error!.userInfo!)")
+            }
+        }
+        [self.textField, becomeFirstResponder()]
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
                // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
